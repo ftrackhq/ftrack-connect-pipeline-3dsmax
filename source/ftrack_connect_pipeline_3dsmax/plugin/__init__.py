47353b5138a91016f62fd1dd4424951379df5043
Lluis Casals <lluis.casals@ftrack.com>
2019-11-20 15:33:02 +0100

    testing run in main thread

--- source/ftrack_connect_pipeline_3dsmax/plugin/__init__.py
+++ source/ftrack_connect_pipeline_3dsmax/plugin/__init__.py
@@ -4,10 +4,17 @@
 from ftrack_connect_pipeline import plugin
 from ftrack_connect_pipeline import constants
 from ftrack_connect_pipeline_3dsmax import constants as max_constants
+from ftrack_connect_pipeline.utils import MainThreadWorker
 
 
 class _BaseMax(plugin._Base):
     host = max_constants.HOST
+
+    def _run(self, event):
+        super_fn = super(_BaseMax, self)._run
+        mThreadWorker = MainThreadWorker()
+        result = mThreadWorker.execute_in_main_thread(super_fn, event)
+        return result
 
 
 class BaseMaxPlugin(plugin.BasePlugin, _BaseMax):
